dc446c9d0dbc2e8b011200a3977f4b416762b576
Piyush <piyush.agarwal01@nagarro.com>
2020-06-18 02:28:51 +0530

    minor change

--- src/app/core/cognito-service/cognito-user.service.ts
+++ src/app/core/cognito-service/cognito-user.service.ts
@@ -190,7 +190,7 @@ export class CognitoUserService {
     localStorage.setItem(CognitoUserService.ID_TOKEN_KEY, user.getIdToken().getJwtToken());
     localStorage.setItem(CognitoUserService.REFRESH_TOKEN_KEY, user.getRefreshToken().getToken());
     console.log("55");
-    console.log(localStorage.getItem('CognitoUserService.ID_TOKEN_KEY'));
+    console.log(localStorage.getItem(CognitoUserService.ID_TOKEN_KEY));
 
   }
 
